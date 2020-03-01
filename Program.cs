2baec313ed5c6c2a0ced8a9463e464f28b8ae11a
Huang Grace <huang.yahui.grace@gmail.com>
2020-03-01 13:54:07 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grace第一支在macos開發的.net core程式.");
+        }
+    }
+}
